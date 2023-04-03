2795c412ba2ea8aaa4e63ef290da5cbace6e6f36
arpentnoir <namlleps.drahcir@gmail.com>
2023-04-03 11:43:14 +1000

    fix: change link to demo site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://vckit.io">
+            to="https://test.uncefact.org">
             Try the demo
           </Link>
         </div>
