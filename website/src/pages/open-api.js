ea90aa481e0e325911ea3e39935e7d47e621058c
arpentnoir <namlleps.drahcir@gmail.com>
2023-05-04 20:25:52 +1000

    chore: resolve linting issue in rapidoc markup

--- website/src/pages/open-api.js
+++ website/src/pages/open-api.js
@@ -5,7 +5,7 @@ const ApiDocs = () => {
   return (
     <Layout title="VC Kit API Docs" description="vc-kit api documentation">
       <iframe
-        src="http://localhost:55883/badges"
+        src="/vckit/html/rapidoc.html"
         frameborder="0"
         style={{position: 'absolute', height: '100%', width: '100%'}}></iframe>
     </Layout>
